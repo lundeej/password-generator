686f7fddb2201056eaa2ede2d4be2c644833138f
Jesenia Lundeen <jeseniathenotary@gmail.com>
2022-06-09 21:56:44 -0700

    adding functions

--- script.js
+++ script.js
@@ -5,25 +5,46 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  var length = Number
+  
   passwordText.value = password;
+  
+}
+// prompt for the length of the password 
 
+//ask for character types 
+
+// lowercase 
+
+// uppercase 
+
+// numeric 
+
+// special charactor 
+
+// function to get lowercase 
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() *26) + 97);
+}
+
+// function to get uppercase 
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) +65);
+}
+
+// function to get number 
+function getRandomNumber() {
+ return String.fromCharCode(Math.floor(Math.random() * 10) +48);
+}
+
+// function to get symbol 
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*(){}[]=<>?,.';
+  return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
 // Defining generatePassword 
 function generatePassword () {
-  // prompt for the length of the password 
-
-  //ask for character types 
-
-  // lowercase 
-
-  // uppercase 
-  
-  // numeric 
-
-  // special charactor 
-
 
   return password;
 }
