cfd96950e837816f8ee6e9f6ca3d262a33c3db2c
Jesenia Lundeen <jeseniathenotary@gmail.com>
2022-06-11 20:56:24 -0700

    adding variable arrays, and function

--- script.js
+++ script.js
@@ -13,3 +13,31 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//Upper case letters
+var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+//Lower case letters
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
+
+// Numbers
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+//Special Characters 
+var specialcharcters = ["!", "@", "#", "$", "%", "^", "&", "*,"]; 
+
+//generate password
+function generatePassword(){
+
+  // Prompt to chose the password length and store in a variable 
+  var length = prompt("Enter the length of the password"); 
+  // validate the password length, by verify that it is between 8 and 128 
+
+  // Prompt user if they want uppercase and store in a variable 
+
+  // Prompt user if they want lowercase and store in a variable 
+
+  // Prompt user if they want numbers and store in a variable 
+
+  // Prompt user if they want special characters and store in a variable 
+
+}
